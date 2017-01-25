72cd85cf41a81faf19499798c9995af739c4673f
Ahmad Andria <andriaahmad@gmail.com>
2017-01-25 12:22:46 +0700

    Kembali ke master dan buat method cariLuasSegitiga

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -2,6 +2,9 @@ public class Hitungluas2{
 	public int cariLuasPersegiPanjang(int panjang, int lebar){
 		return panjang * lebar;
 	}
+	public int cariLuasSegitiga(int alas, int tinggi){
+		return alas * tinggi / 2;
+	}
 	public static void main(String[]args){
 	}
 }
