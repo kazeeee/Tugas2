de67a32f5be355d439e047772d5be0cc13641610
Ahmad Andria <andriaahmad@gmail.com>
2017-01-25 12:18:59 +0700

    Pembuatan Branch LuasLingkaran dan Method CariLuasLingkaran

--- HitungLuas2.java
+++ HitungLuas2.java
@@ -2,6 +2,9 @@ public class Hitungluas2{
 	public int cariLuasPersegiPanjang(int panjang, int lebar){
 		return panjang * lebar;
 	}
+	public int cariLuasLingkaran(){
+		return 22/7 * jarijari * jarijari;
+	}
 	public static void main(String[]args){
 	}
 }
